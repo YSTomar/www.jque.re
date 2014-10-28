43961c410a98c4dd152eba1e166e6e2cc92a1d45
David Higgins <alertdebug@higg.im>
2014-10-28 01:37:36 +0000

    _

---
+++ js/init.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function () {
+
+    $("#container").css("height", $(window).height());
+    $(window).resize(function () {
+        $("#container").css("height", $(window).height())
+    });
+    $("#receiver").css("width", $(window).width() - 400);
+    $(window).resize(function () {
+        $("#receiver").css("width", $(window).width() - 400)
+    })
+})
